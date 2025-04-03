aec67bde2d47c8e67c7e02057f64d53999dc6763
Thomas Crochet <thomas@gmail.com>
2025-04-03 10:17:01 +0200

    Remove template vars

--- src/agent.ts
+++ src/agent.ts
@@ -1,19 +1,22 @@
-import { blModel, blTools, logger } from '@blaxel/sdk';
-import { streamText, tool } from 'ai';
-import { z } from 'zod';
+import { blModel, blTools, logger } from "@blaxel/sdk";
+import { streamText, tool } from "ai";
+import { z } from "zod";
 
 interface Stream {
   write: (data: string) => void;
   end: () => void;
 }
 
-export default async function agent(input: string, stream: Stream): Promise<void> {
+export default async function agent(
+  input: string,
+  stream: Stream
+): Promise<void> {
   const response = streamText({
     experimental_telemetry: { isEnabled: true },
-    model: await blModel("{{.Model}}").ToVercelAI(),
+    model: await blModel("sandbox-openai").ToVercelAI(),
     tools: {
-      ...await blTools(['blaxel-search']).ToVercelAI(),
-      "weather": tool({
+      ...(await blTools(["blaxel-search"]).ToVercelAI()),
+      weather: tool({
         description: "Get the weather in a specific city",
         parameters: z.object({
           city: z.string(),
@@ -25,9 +28,7 @@ export default async function agent(input: string, stream: Stream): Promise<void
       }),
     },
     system: "If the user ask for the weather, use the weather tool.",
-    messages: [
-      { role: 'user', content: input }
-    ],
+    messages: [{ role: "user", content: input }],
     maxSteps: 5,
   });
 
